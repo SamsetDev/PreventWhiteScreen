c443fb9c8e932a1c59d5cdad6c28a0e3e359fe75
SamsetDev <singh.891sanjay@gmail.com>
2016-06-08 17:32:58 +0530

    Splash
    Splash

---
+++ Splash/app/src/main/java/com/samset/splash/Splash.java
@@ -0,0 +1,25 @@
+package com.samset.splash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(Splash.this,MainActivity.class));
+            }
+        }).start();
+    }
+}
